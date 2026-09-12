bbbfd10ffe6e3bfbf4d0d9df40b72869cd55c918
Prakhar <tiearireyansh17@gmail.com>
2026-09-12 15:09:23 +0530

    added routing and controller layer

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import {config} from "./config/index.js";
+import {pingHandler} from "./controllers/ping.controller.js";
 const app: Express = express();
+import pingRouter from "./routers/ping.router.js";
 
-app.get('/ping', (req: any, res: any) => {
-    res.send('pong')
-})
-
+// app.get('/ping', pingHandler)/
+// /This line is working like a router
+app.use(pingRouter);
+// you are registering all the routes with the router
 app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
 });
